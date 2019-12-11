190de59cf65a44ef9afa32e8a59a1cb083b9cc33
Juan Snyman <git@jsnyman.com>
2019-12-11 16:02:11 +0000

    Create basic layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
+//= require onmount
 //= require_tree .
+
+// Cause JavaScript behaviours to run whenever a new page is loaded by Turbolinks
+$(document).on('ready page:change', function () { $.onmount() })

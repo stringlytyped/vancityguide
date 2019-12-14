b5b47e6197cf4b1583205f3a0cbbc601a2f8673e
Juan Snyman <git@jsnyman.com>
2019-12-14 11:27:10 +0000

    Add authentication using Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new
+user.email = "admin@example.com"
+user.password = "password"
+user.password_confirmation = "password"
+user.admin = true
+user.save!
+
+puts "Created default admin user"

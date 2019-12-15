ff72c2b4841b9628a1ec4231f7b3fd543b834aff
Juan Snyman <git@jsnyman.com>
2019-12-15 21:39:32 +0000

    Add content to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_featured_pois
+
+  private
+
+    def set_featured_pois
+      @featured_pois = Poi.where(featured: true, public: true)
+    end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def home
+    @user_pois = Poi.where(public: true, featured: false)
   end
 end
